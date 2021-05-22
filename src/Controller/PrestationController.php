16c8c3a92e24b055ddf8826c0778b9dcc5e03a0d
Yannick Biheul <yannickbiheul@outlook.fr>
2021-05-22 10:51:45 +0200

    factoring

--- src/Controller/PrestationController.php
+++ src/Controller/PrestationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
 use App\Repository\UserRepository;
 use App\Repository\GeneralRepository;
 use App\Repository\PrestationRepository;
@@ -15,11 +16,13 @@ class PrestationController extends AbstractController
      * @Route("/prestation", name="prestation")
      */
     public function index(GeneralRepository $generalRepository, 
-    UserRepository $userRepository, PrestationRepository $prestationRepository): Response
+    UserRepository $userRepository, PrestationRepository $prestationRepository,
+    CategorieRepository $categorieRepository): Response
     {
         $general = $generalRepository->findOneBy(['logo' => 'logo.png']);
         $admin = $userRepository->findOneBy(['nom' => 'Fourel']);
         $prestations = $prestationRepository->findAll();
+        $categories = $categorieRepository->findAll();
         return $this->render('prestation/index.html.twig', compact('general', 'admin', 'prestations'));
     }
 }
